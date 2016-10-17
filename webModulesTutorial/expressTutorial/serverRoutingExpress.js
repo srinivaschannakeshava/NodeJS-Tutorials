742e20e9f9445343c8f40a575aa92d5584194859
srinivaschannakeshava <srinivaschannakeshava@gmail.com>
2016-10-17 19:45:37 +0530

    upload tutorials

--- webModulesTutorial/expressTutorial/serverRoutingExpress.js
+++ webModulesTutorial/expressTutorial/serverRoutingExpress.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 var app = express();
+var upload = multer();
 app.use(express.static('public'));
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
+
+// Create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(multer({
+    dest: './tmp/'
+}).single('updfile'));
+
 
 app.get('/pages/:pageName', function(req, res) {
     var fileName = __dirname + "/public/html/" + req.params.pageName + ".html"
@@ -16,15 +24,44 @@ app.get('/pages/:pageName', function(req, res) {
             res.status(err.status).send("404 Not Found");
         }
     });
-    /*fs.readFile(fileName, function(err, data) {
-        if (err) {
-            res.send("Doesn't exist"+err);
-        } else {
-          res.set('Content-Type', 'text/HTML');
-            res.send(data);
-        }
-    })*/
+});
+app.get('/formgetdata', function(req, res) {
+    resp = {
+        fName: req.query.first_name,
+        lName: req.query.last_name
+    }
+    res.end(JSON.stringify(resp));
 })
+
+app.post('/formpostdata', function(req, res) {
+    resp = {
+        fName: req.body.first_name,
+        lName: req.body.last_name
+    }
+    res.end(JSON.stringify(resp));
+})
+
+app.post('/fileupload', function(req, res) {
+    console.log(req.file.originalname);
+    console.log(req.file.path);
+    console.log(req.file.mimetype);
+    var file = __dirname + '/uploads/' + req.file.originalname;
+    fs.readFile(req.file.path, function(err, data) {
+        fs.writeFile(file, data, function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                response = {
+                    message: 'File uploaded successfully',
+                    filename: req.file.originalname
+                };
+            }
+            console.log(response);
+            res.end(JSON.stringify(response));
+        });
+    });
+})
+
 app.listen(8080, function() {
     console.log("Server started @port 8080");
 })
